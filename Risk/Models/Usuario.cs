edf9a99176a179c0729e31938ee29bb5f1cd3e1a
vaio <vaio@VAIO-PC>
2016-04-04 18:01:00 +0200

    Comenzando Login

---
+++ Risk/Models/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Risk.Models
+{
+    public class Usuario
+    {
+
+        public string _usuario
+        {
+            get
+            {
+                return _usuario;
+            }
+
+            set
+            {
+                _usuario = value;
+            }
+        }
+
+        public string _clave
+        {
+            get
+            {
+                return _clave;
+            }
+
+            set
+            {
+                _clave = value;
+            }
+        }
+    }
+}
