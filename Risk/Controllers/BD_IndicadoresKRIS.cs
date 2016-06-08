0741d2b9d2942604aaa02de8752a9df007c213f1
Sony <Sony@SONY-PC>
2016-06-08 14:55:45 +0200

    BD_Riesgos. Codigo comentado. Posible borrado del comentado

---
+++ Risk/Controllers/BD_IndicadoresKRIS.cs
@@ -0,0 +1,13 @@
+﻿using Risk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Risk.Controllers
+{
+    public class BD_IndicadoresKRIS
+    {
+        Consultas_BDDataContext Conexion = (Consultas_BDDataContext)new ConnectionDB.connectionGeneral().connectionGeneralConsultas();
+    }
+}
