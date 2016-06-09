31be9e4fd4fd482664d4fb10ae2bbbf6e923ca1b
Maribel <aulalcala16-2@hotmail.com>
2016-06-09 15:03:09 +0200

    Scoope Kris

--- Risk/Controllers/KRISController.cs
+++ Risk/Controllers/KRISController.cs
@@ -33,41 +33,14 @@ namespace Risk.Controllers
             return PartialView(indicadorRecup);
         }
 
-        public ActionResult Scoope()
+        public ActionResult Scoope(int id)
         {
-            // 3 tablas: riesgos, planes y localizacion
-
-            TablaRiesgos_Risks tablaRiesgos = new TablaRiesgos_Risks();
-            Dictionary<string, object> filtro = new Dictionary<string, object>();
-            DatosTablaModel modelTablaRiesgos = tablaRiesgos.dameTabla(filtro);
-            modelTablaRiesgos.colVer = "CodRiesgo,CodRiesgoLocalizado,Nombre,Categoria,Clasif1,Clasif2,Clasif3";
-            modelTablaRiesgos.colTitulo = "CodRiesgo,CodRiesgoLocalizado,Nombre,Categoria,Clasif1,Clasif2,Clasif3";
-
-
-            string colVer = "CodRiesgo,Nombre,Categoria,Clasif1,Clasif2,Clasif3,CodRiesgoLocalizado";
-            string colTitulos = "Código Riesgo,Nombre,Categoría,Clasificación1,Clasificación2,Clasificación3,Código Localizado";
-
-            //DatosTablaModel datosTabla = new DatosTablaModel();
-            //datosTabla.datosTHead = BD_Riesgos.nombresColTabla("qRiesgosNombres", colVer, colTitulos);
-
-            //Dictionary<int, List<Tuple<string, string>>> dicBody = new Dictionary<int, List<Tuple<string, string>>>();
-            //Dictionary<int, List<Tuple<string, string>>> dic = BD_Riesgos.cargaTablaDatos("qRiesgosNombres", colVer, colTitulos);
-
-            //foreach (var item in dic.Take(3))
-            //{
-            //    dicBody.Add(item.Key, item.Value);
-            //}
-
-            //datosTabla.datosTBody = dicBody;
-            //datosTabla.vistaProcedencia = "Scoopes";
-            //datosTabla.editable = false;
-            //datosTabla.borrar = false;
-
+            
             TablaRiesgos_Risks tabla = new TablaRiesgos_Risks();
             Dictionary<string, object> filtros = new Dictionary<string, object>();
             DatosTablaModel tablaR = tabla.dameTabla(filtros);
 
-            return PartialView(tabla);
+            return PartialView(tablaR);
         }
 
         public ActionResult Archive(int id)
@@ -77,13 +50,7 @@ namespace Risk.Controllers
 
             TablaIndicadores_KRIS tablaIndicadores = new TablaIndicadores_KRIS(colVer,colTitulo);
             DatosTablaModel modelTablaIndicadores = tablaIndicadores.dameTablaPorIdIndicador(id);
-        
-            //modelTablaIndicadores.datosTHead = BD_MontaDatosTabledata.nombresColTabla("qIndicadores", modelTablaIndicadores.colVer, modelTablaIndicadores.colTitulo);
-            //Dictionary<string, string> cabeceras = BD_MontaDatosTabledata.nombresColTabla("qIndicadores", modelTablaIndicadores.colVer, modelTablaIndicadores.colTitulo);
-            //modelTablaIndicadores.datosTBody = BD_MontaDatosTabledata.cargaTBody(dicTBody,cabeceras);
-           
-            //modelTablaIndicadores.titulo = "";
-            //modelTablaIndicadores.editable = false;
+              
 
             return PartialView(modelTablaIndicadores);
         }
